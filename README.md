abf3a0290c80b72166c884845bc8e0e505f58fc6
Carlos M. Fernández <cfernandez@coredial.com>
2018-02-02 17:59:14 -0500

    First version

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# erizomon
+# erizomon.py
 Monitor Licode Erizo errors and send alerts
+
+# Usage
+Simply set the time range, SMTP server, sender, and recipients in the file config/config.json and run the script.
+Preferably this script will be run from cron on a regular schedule, e.g. every 10 minutes, and the time range in
+the config file would match that.
---
+++ erizomon.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+import sys
+import json
+import smtplib
+from email.mime.text import MIMEText
+from elasticsearch import Elasticsearch, helpers
+
+def get_config_path( argv ):
+    if len( argv ) is 0:
+        return "./config/config.json"
+    else:
+        return argv[0]
+
+def get_config( path ):
+    try:
+        cfile = open( path, 'r' )
+        config = json.load( cfile )
+        cfile.close()
+        return config
+    except IOError:
+        sys.exit( "Cannot open configuration file: {0}".format( path ) )
+
+def get_log_scanner( config ):
+    es = Elasticsearch( config['elasticsearch'] )
+    res = helpers.scan( client = es, scroll = "2m", query = config['query'], index = config["index"] )
+    return res
+
+def main( argv ):
+    config = get_config( get_config_path( argv ) )
+    alert = False
+    errmsg = None
+    errtime = None
+    for hit in get_log_scanner( config ):
+        if "code: 503" in hit['fields']['message'][0]:
+            alert = True
+            errmsg = hit['fields']['message'][0]
+            errtime = hit['fields']['@timestamp'][0]
+    if alert:
+        mailer = smtplib.SMTP( config['smtphost'] )
+        msg = MIMEText( config['msgtext'].format( errtime, errmsg ) )
+        msg['Subject'] = config['msgsubject']
+        msg['From'] = config['sender']
+        msg['To'] = ", ".join( config['recipients'] )
+        mailer.sendmail( config['sender'], config['recipients'], msg.as_string() )
+        #print msgtext
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
